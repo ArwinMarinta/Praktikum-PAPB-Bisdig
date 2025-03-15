f3df8b0140300357cb0d1a5aa4bec1e852f94be3
Arwin Marinta <11211019@student.itk.ac.id>
2025-03-15 13:25:33 +0800

    a

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -98,7 +98,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     // mainAxisAlignment: MainAxisAlignment.start,
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(
+                      const Text(
                         'How Are You Feeling Today?',
                         style: TextStyle(
                             fontSize: 14.0, fontWeight: FontWeight.w400),
